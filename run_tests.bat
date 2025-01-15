e4cf9b235593f150ac80833bdc3771c5c5ed8ce9
Evandro Santana <evandrosant@gmail.com>
2025-01-14 21:34:00 -0300

    Tests update

--- run_tests.bat
+++ run_tests.bat
@@ -3,14 +3,17 @@
 :: Navegar até o diretório do projeto de testes
 cd src\OutseraMovies.Tests
 
-:: Restaurar dependências (opcional, caso necessário)
+:: Restaurar dependências
 echo Restaurando dependências...
 dotnet restore
 
-:: Executar os testes
+:: Compilar o projeto de testes
+echo Compilando o projeto de testes...
+dotnet build
+
+:: Executar os testes (sem rebuild)
 echo Executando os testes...
 dotnet test --no-build --verbosity normal
 
 :: Manter o terminal aberto
 pause
-
